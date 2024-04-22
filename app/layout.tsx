ce129580c15710ebe9ba2850d8ba782828d6eefc
xiaoqingqing <1164265080@qq.com>
2024-04-22 19:47:43 +0800

    init: 初始化项目

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "chaoyue",
+  description: "程超越的web端",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
